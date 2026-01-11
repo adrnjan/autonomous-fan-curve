b2a0976013086d31f1b4ffc0f76430c8f5a94b45
adrian <113150668+adrnjan@users.noreply.github.com>
2026-01-10 21:35:16 -0500

    Add files via upload

--- math_calculations.c
+++ math_calculations.c
@@ -2,28 +2,46 @@
 #include "thermallogic.h"
 #include <windows.h>
 
-void calculate_fan_curve(){
-    struct readings state; 
-    struct math_variables settings;
-    settings.emaAlpha = 0.1f; //adjust later
+void calculate_fan_curve(struct readings* state, struct math_variables* settings){
+   settings->emaAlpha = 0.1f; //adjust later
    int first_run = 0;
-   
-   while(1){
-   state.rawtemp = retrieve_temp_reading();
+   float base_speed = 0;
+   float final_speed = 0;
+   state->rawtemp = retrieve_temp_reading();
 
-   if (first_run==0){
+   if (state->isFirstRun==0){
     first_run = 1;
-    state.currentEMA = state.rawtemp;
+    state->currentEMA = state->rawtemp;
    }
    else
-   state.currentEMA = (settings.emaAlpha * state.rawtemp) + ((1-settings.emaAlpha) * state.lastEMA);
+   state->currentEMA = (settings->emaAlpha * state->rawtemp) + ((1 - settings->emaAlpha) * state->lastEMA);
 
-    //fan_curve base speed (likely will be most confusing part, set by user. needs to be such that there is a balance between sound and perf)
+    //fan_curve base speed (linear, will be placed at 20% between 0 and 40c, will be linear increase 20%-50% from 40c-70c, 75 percent at all times above 70)
+    if (state->currentEMA < 40){
+        base_speed = 20;
+    }
+    else if (state->currentEMA > 40 && state->currentEMA < 70){
+        base_speed = 20 + (state->currentEMA-40);
+    }
+    else
+    base_speed = 75;
 
-    settings.slope = state.currentEMA - state.lastEMA;
-    settings.boost = 0;
-    state.lastEMA = state.currentEMA;
+    settings->slope = state->currentEMA - state->lastEMA;
+    settings->boost = 0;
+    state->lastEMA = state->currentEMA;
 
-    Sleep(1000); 
-   }
+    
+    if (state->currentEMA>60 && settings->slope > 2.0){
+        settings->boost = settings->slope * 1.5;
+        //base fan curve + boost factor, or maybe there is a better way to apply the boost
+    }
+
+    
+    final_speed = base_speed + settings->boost;
+        if (final_speed + settings->boost > 100){
+        base_speed = 100;
+    }
+    elseif (final_speed + settings->boost < 0){
+        base_speed = 0;
+
 }
